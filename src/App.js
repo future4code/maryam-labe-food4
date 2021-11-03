d9f5c4379dcfa71cc01dd39d95af81e696cfa75e
Rafael Cacilhas <88065564+rafaelcacilhas@users.noreply.github.com>
2021-11-03 10:08:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
-import SearchPage from './pages/SearchPage/SearchPage';
-import RestaurantDetails from './pages/RestaurantDetails/RestaurantDetails'
+import React            from 'react';
+import GlobalState      from "./contexts/GlobalState";
+import { GlobalStyle }  from "./GlobalStyle"
 
-const GlobalStyle = createGlobalStyle`
-body{
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-  background-color: #ffffff;
-  margin: 0;
-  font-family: "SulSans", Helvetica, sans-serif;
-  font-size: 1em;
-  font-weight: 500;
-}
-div{
-  box-sizing: border-box;
-  height: 100vh;
-  width: 100vw;
-  max-width: 400px;
-  flex-shrink: 0;
-}
-`;
+import Router           from "./routes/Router";
+import theme            from "./constants/theme";
+
+import {ThemeProvider}  from '@mui/material/styles';
+import CssBaseline      from "@mui/material/CssBaseline";
+
 
 const App = () => {
   return (
-    <>
-      <GlobalStyle />
-      <SearchPage/>
-    </>
+    <GlobalState>
+
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+        <GlobalStyle />
+        <Router />
+
+      </ThemeProvider>
+
+    </GlobalState>
   );
-}
+};
 
 export default App;
+
