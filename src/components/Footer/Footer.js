8c907f887f0b2db64f93fa611822cc4a5791f8a7
Matheus Guilherme Schade <matheus_schade@hotmail.com>
2021-11-05 09:58:58 -0300

    corrigido erros da última versão

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,61 +1,33 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 import React from 'react';
-
-import {goToSearch}   from "../../routes/coordinator";
-import {goToProfile}  from "../../routes/coordinator";
-import {goToCart}     from "../../routes/coordinator";
-
-import {useHistory}   from 'react-router-dom';
-
-import BottomNavigation       from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import HomeIcon               from '@mui/icons-material/Home';
-import ShoppingCartIcon       from '@mui/icons-material/ShoppingCart';
-import PersonIcon             from '@mui/icons-material/Person';
-import Paper                  from '@mui/material/Paper';
-
->>>>>>> 80672183275f76479cdcbbacd4dd7cb099b15d03
-
-import { goToHome } from "../../routes/coordinator";
 import { goToProfile } from "../../routes/coordinator";
-import { goToCart } from "../../routes/coordinator";
-
-import { useHistory } from "react-router-dom";
-
-import BottomNavigation from "@mui/material/BottomNavigation";
+import { goToCart, goToHome } from "../../routes/coordinator";
+import { useHistory } from 'react-router-dom';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
+import Paper from '@mui/material/Paper';
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import HomeIcon from "@mui/icons-material/Home";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PersonIcon from "@mui/icons-material/Person";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
+// import HomeIcon from "@mui/icons-material/Home";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import PersonIcon from "@mui/icons-material/Person";
+// import Paper from "@mui/material/Paper";
+
 
 const Footer = () => {
-<<<<<<< HEAD
   const [value, setValue] = React.useState(0);
-  const history = useHistory();
+  const history = useHistory()
   return (
-    <Paper
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+    <Paper sx={{
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      right: 0
+    }}
       elevation={3}
     >
-      <BottomNavigation
-=======
-    const [value, setValue] = React.useState(0);
-    const history = useHistory()
-    return (
-    <Paper sx={{ 
-        position: 'fixed', 
-        bottom: 0, 
-        left: 0, 
-        right: 0 }} 
-    elevation={3}
-    >
 
-        <BottomNavigation
->>>>>>> 80672183275f76479cdcbbacd4dd7cb099b15d03
+      <BottomNavigation
         showLabels
         value={value}
         onChange={(event, newValue) => {
